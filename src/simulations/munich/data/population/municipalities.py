35486bb16562bea2e0bfab6bdd81e12282007fa7
enatterer <elena.natterer@tum.de>
2025-03-20 20:35:49 +0100

    added config bavaria run simulations and other files which were created previously

---
+++ src/simulations/munich/data/population/municipalities.py
@@ -0,0 +1,21 @@
+"""
+Generates a data frame with population count per municipality in Germany.
+"""
+
+def configure(context):
+    context.stage("munich.data.population.raw")
+
+def execute(context):
+    # Load shapes
+    df = context.stage("munich.data.population.raw")[["municipality_code", "population"]]
+
+    # Clean up identifiers
+    df["region_id"] = df["municipality_code"].str[:2].astype("category")
+    df["departement_id"] = df["municipality_code"].str[:5].astype("category")
+    df["commune_id"] = df["municipality_code"].astype("category")
+
+    # Fake IRIS
+    df["iris_id"] = df["commune_id"].astype(str) + "0000"
+    df["iris_id"] = df["iris_id"].astype("category")
+
+    return df[["region_id", "departement_id", "commune_id", "iris_id", "population"]]
